71309849b1fe81ce68dbb0a5cf8802bfa7abd7f4
Takeru Hayashi <takeruhayashi@g.ucla.edu>
2025-12-19 16:31:25 -0800

    UPDATE: demo/tps55287q1_dev

--- demo/tps55287q1_dev/src/main.c
+++ demo/tps55287q1_dev/src/main.c
@@ -44,23 +44,48 @@ int main(void) {
     }
     LOG_INF("Current limit is ~%f A (%d uA)", (double)curr_limit_ua / 1000000, curr_limit_ua);
 
-    /* Example: set 5 V */
-    ret = regulator_set_voltage(tps, 5000000, 5000000);
-    if (ret < 0) {
-        LOG_ERR("regulator_set_voltage() failed: %d", ret);
-        return 0;
+    // /* Example: set 5 V */
+    // ret = regulator_set_voltage(tps, 5000000, 5000000);
+    // if (ret < 0) {
+    //     LOG_ERR("regulator_set_voltage() failed: %d", ret);
+    //     return 0;
+    // }
+
+    // int32_t vout_uv;
+    // ret = regulator_get_voltage(tps, &vout_uv);
+    // if (ret < 0) {
+    //     LOG_ERR("regulator_get_voltage() failed: %d", ret);
+    //     return 0;
+    // }
+    // LOG_INF("VOUT set to ~%d mV", (int)(vout_uv / 1000));
+
+
+    int32_t voltages_uv[] = {
+        1000000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000, 8000000, 9000000, 10000000, 11000000, 12000000,
+    };
+    const size_t num_voltages = sizeof(voltages_uv) / sizeof(voltages_uv[0]);
+
+    for (size_t i = 0; i < num_voltages; i++) {
+        int32_t voltage_uv = voltages_uv[i];
+        ret = regulator_set_voltage(tps, voltage_uv, voltage_uv);
+        if (ret < 0) {
+            LOG_ERR("regulator_set_voltage() failed: %d", ret);
+            return 0;
+        }
+
+        int32_t vout_uv;
+        ret = regulator_get_voltage(tps, &vout_uv);
+        if (ret < 0) {
+            LOG_ERR("regulator_get_voltage() failed: %d", ret);
+            return 0;
+        }
+        LOG_INF("VOUT set to ~%f V (%d mV)", (double)vout_uv / 1000000.0, (int)(vout_uv / 1000));
+
+        k_sleep(K_SECONDS(3));
     }
 
-    int32_t vout_uv;
-    ret = regulator_get_voltage(tps, &vout_uv);
-    if (ret < 0) {
-        LOG_ERR("regulator_get_voltage() failed: %d", ret);
-        return 0;
-    }
-    LOG_INF("VOUT set to ~%d mV", (int)(vout_uv / 1000));
-
-    LOG_INF("Sleeping for 10 seconds...");
-    k_sleep(K_SECONDS(10));
+    LOG_INF("Sleeping for 5 seconds...");
+    k_sleep(K_SECONDS(5));
 
     LOG_INF("Disabling TPS55287Q1...");
     ret = regulator_disable(tps);
